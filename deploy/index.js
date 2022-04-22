031ad373c6668b727cfe0636160c8b0c5009b837
irsyadulibad <ahmadirsyadulibad7@gmail.com>
2022-04-22 15:40:47 +0700

    feat: deploy to server with FTP

---
+++ deploy/index.js
@@ -0,0 +1,28 @@
+require('dotenv').config({path: 'deploy/.env'});
+const ftpDeploy = require('ftp-deploy');
+
+const ftp = new ftpDeploy;
+
+const env = name => {
+    name = name.toUpperCase();
+    return process.env[name];
+}
+
+const config = {
+    user: env('ftp_username'),
+    password: env('ftp_password'),
+    host: env('ftp_host'),
+    port: env('ftp_port'),
+    localRoot: __dirname + '/../public',
+    remoteRoot: '/',
+    include: ["*", "**/*"],
+    deleteRemote: true,
+    sftp: false
+}
+
+ftp.deploy(config)
+    .then(res => {
+        console.log('Succeeded')
+    }).catch(err => {
+        console.log(err)
+    });
